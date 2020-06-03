c8479af30825fc2c2b0ba932177e8c1e07cf1eb8
Roman Opalko <roman.opalko@2sic.com>
2020-06-03 16:55:21 +0200

    clean up, removed gulp, webpack, razorcomponent

--- dist/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".app-categories").click(function(e) {
-        e.stopPropagation();
-    });
-    $(".app-event-item .btn.btn-primary").click(function(e) {
-        e.stopPropagation();
-    });
-});
