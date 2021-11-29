ac6f6673d42e8800f24b2183460892f38ecb77b1
Maxim Hüsler <maxim.huesler@2sic.com>
2021-11-29 17:03:41 +0100

    minor lib files

--- src/ts/lib-2sxc-recaptcha.ts
+++ src/ts/lib-2sxc-recaptcha.ts
@@ -4,6 +4,7 @@ declare let grecaptcha: any;
   This is a shared code used in various 2sxc apps. Make sure that they are in sync, so if you improve it, improve all 2sxc apps which use this. 
   ATM they are:
   - EventsAndCourses6
+  - MobiusForms5
   The master with the newest / best version must always be the Gallery7, the others should then get a fresh copy.
   Because this is shared, all parameters like DOM-IDs etc. must be provided in the Init-call that it can work across apps
 */ 
