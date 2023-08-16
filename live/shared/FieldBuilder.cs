250ce38cc28babef589bdc89c2e7cdeb6845c083
Roman Opalko <roman.opalko@2sic.com>
2023-08-16 09:22:42 +0200

    minors, staging -> live

--- live/shared/FieldBuilder.cs
+++ live/shared/FieldBuilder.cs
@@ -55,15 +55,20 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     // Add a placeholder text to the inputs
     internal string PhLabel(string key, bool required)
     {
-        return LabelInPlaceholder ? App.Resources.String("Label" + key) + (required ? "*" : "") : "";
+      return LabelInPlaceholder ? App.Resources.String("Label" + key, scrubHtml: "p") + (required ? "*" : "") : "";
     }
 
     // Sets a RazorBlade Input/TextArea to required and adds the message which is different for each field type
-    internal void SetRequired(IHtmlTag item, bool required, string message) 
+    internal void SetRequired(IHtmlTag item, bool required, string key = null)
     {
         if (!required) return;
+        var message = key != null
+          ? App.Resources.String(key)
+          : (MessageRequired ?? (MessageRequired = App.Resources.String("LabelRequired")));
         item.Attr("data-pristine-required-message", message).Attr("required");
     }
+    // Cache the message after first lookup for performance as we use it quite ofter
+    private string MessageRequired = null;
 
 
     // returns a Label
@@ -77,7 +82,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     {
         var item = Tag.Input().Type("text").Id(idString).Placeholder(PhLabel(idString, required)).Class("form-control");
         if (value != null) { item.Attr("value", value); }
-        SetRequired(item, required, App.Resources.String("LabelRequired"));
+        SetRequired(item, required);
         if (disabled) { item = item.Disabled(); }
         return Field(idString, required, item);
     }
@@ -85,7 +90,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     public IHtmlTag EMail(string idString, bool required)
     {
         var item = Tag.Input().Type("email").Id(idString).Placeholder(PhLabel(idString, required)).Class("form-control");
-        SetRequired(item, required, App.Resources.String("LabelValidEmail"));
+        SetRequired(item, required, "LabelValidEmail");
         return Field(idString, required, item);
     }
 
@@ -94,7 +99,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     {
         var item = Tag.Textarea().Id(idString).Placeholder(PhLabel(idString, required)).Class("form-control");
         if (value != null) { item.Add(value); }
-        SetRequired(item, required, App.Resources.String("LabelRequired"));
+        SetRequired(item, required);
         if (disabled) { item = item.Disabled(); }
         return Field(idString, required, item);
     }
@@ -103,7 +108,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     public IHtmlTag DropDown(string idString, bool required, string[] values)
     {
         var item = Tag.Select().Id(idString).Class("form-control");
-        SetRequired(item, required, App.Resources.String("LabelRequired"));
+        SetRequired(item, required);
         item.Add(Tag.Option(App.Resources.String("LabelPleaseSelect")).Attr("value", ""));
         foreach (var value in values)
         {
@@ -121,7 +126,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
             var radioId = idString + value.ToLower().Replace(" ", "");
             var wrapper = Tag.Div().Class(Kit.Css.Is("bs3") ? "radio" : "form-check");
             var radio = Tag.Input().Attr("type", "radio").Id(radioId).Name(idString).Value(value);
-            SetRequired(radio, required, App.Resources.String("LabelRequired"));
+            SetRequired(radio, required);
             if (Kit.Css.Is("bs3")) {
                 var radioLabel = Tag.Label(radio + value).For(radioId);
                 wrapper.Add(radioLabel);
@@ -138,23 +143,23 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     // returns a checkbox with common attributes
     public IHtmlTag Checkbox(string idString, bool required){
         var checkbox = Tag.Input().Attr("type", "checkbox").Id(idString).Name(idString).Class("form-check-input");
-        SetRequired(checkbox, required, App.Resources.String("LabelRequired"));
-        var labelTranslated = Kit.Scrub.Only(App.Resources.String("Label" + idString), "p");
+        SetRequired(checkbox, required);
+        var labelTranslated = App.Resources.String("Label" + idString, scrubHtml: "p");
         var label = ToSic.Razor.Blade.Text.First(labelTranslated, idString) + (required ? "*" : "");
 
         // Slightly different HTML for Bootstrap3
         if (Kit.Css.Is("bs3")) {
-        return Tag.Div().Class(FormValidationClass() + "form-group").Wrap(
-            Tag.Div().Class("checkbox").Wrap(
-            Tag.Label().Wrap(checkbox, label)
-            )
-        ); 
+            return Tag.Div().Class(FormValidationClass() + "form-group").Wrap(
+                Tag.Div().Class("checkbox").Wrap(
+                    Tag.Label().Wrap(checkbox, label)
+                )
+            ); 
         } else {
-        // Bootstrap4 and 5
-        return Tag.Div().Class(FormValidationClass() + "mb-3 form-check" ).Wrap(
-        checkbox,
-        Tag.Label(label).Class("form-check-label").For(idString)
-        );
+            // Bootstrap4 and 5
+            return Tag.Div().Class(FormValidationClass() + "mb-3 form-check" ).Wrap(
+                checkbox,
+                Tag.Label(label).Class("form-check-label").For(idString)
+            );
         }
   }
 
@@ -162,7 +167,7 @@ public class FieldBuilder : Custom.Hybrid.CodeTyped
     private IHtmlTag Field(string idString, bool required, IHtmlTag items)
     {
         var inputWrapperClasses = Kit.Css.Is("bs3") ? "col col-xs-12 col-sm-9" : "col-12  col-sm-9";
-        var labelTranslated = App.Resources.String("Label" + idString);
+        var labelTranslated = App.Resources.String("Label" + idString, scrubHtml: "p", required: false);
         var field = Tag.Div().Class(FormClasses());
 
         // If the label is _not_ in the placeholder, add the label first
