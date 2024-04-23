1d15e1cca221608f4210ff1f1e2729e2440cf4bd
Dominik Graf <dominik.graf@2sic.com>
2024-04-23 11:44:30 +0200

    minor remove fileMap

--- src/ts/lib-2sxc-forms.ts
+++ src/ts/lib-2sxc-forms.ts
@@ -7,17 +7,11 @@ declare let $2sxc: any;
   This is a shared code used in various 2sxc apps. Make sure that they are in sync, so if you improve it, improve all 2sxc apps which use this. 
   ATM they are:
   - EventsAndCourses6
-  - MobiusForms5
+  - MobiusForms6
+  - Jobs
   The master with the newest / best version must always be the Gallery7, the others should then get a fresh copy.
   Because this is shared, all parameters like DOM-IDs etc. must be provided in the Init-call that it can work across apps
 */
-
-const promiseFileMap = (file: { Encoded: Promise<unknown> }) => {
-  if (!file.Encoded) return file;
-  return new Promise((resolve) => {
-    file.Encoded.then((result) => resolve({ ...file, Encoded: result }));
-  });
-};
 
 // automatically build the send-object with all properties,
 // based on all form-fields which have a item-property=""
