978c17702b2b8ecb3e9ff7e4a4c5d99661e17882
Roman Opalko <roman@opalko.de>
2024-06-17 16:04:13 +0200

    staging > live

--- live/AppCode/Razor/AppRazor.cs
+++ live/AppCode/Razor/AppRazor.cs
@@ -17,10 +17,24 @@ namespace AppCode.Razor
     /// <summary>
     /// Shows Event Details boxes
     /// </summary>
-    public IHtmlTag DetailsBox(string label, string copy)
+    public IHtmlTag DetailsBox(string label, string copy, string itemprop = "")
     {
       if (!Text.Has(copy)) return null;
-      return Tag.Div(Tag.H6(label), copy).Class("col-12 col-md-6 mb-3 app-events6-infocontainer");
+      var copyText = Tag.Span(copy);
+
+      if(itemprop == "location") copyText.Attr("itemprop", "address").Attr("itemscope", "").Attr("itemtype", "https://schema.org/PostalAddress");
+      if(itemprop == "performer" || itemprop == "organizer") copyText.Attr("itemprop", "name");
+      if(itemprop == "offers") copyText.Attr("itemprop", "price");
+
+      var tag = Tag.Div(Tag.H6(label), copyText).Class("col-12 col-md-6 mb-3 app-events6-infocontainer");
+      
+      if(itemprop != "") tag.Attr("itemprop", itemprop);
+
+      if(itemprop == "location") tag.Attr("itemscope", "").Attr("itemtype", "https://schema.org/Place");
+      if(itemprop == "performer") tag.Attr("itemscope", "").Attr("itemtype", "https://schema.org/PerformingGroup");
+      if(itemprop == "offers") tag.Attr("itemscope", "").Attr("itemtype", "https://schema.org/Offer");
+
+      return tag;
     }
 
     /// <summary>
